f099d156054c7ee333578b4b36766f008b185d8f
ntphat11296 <32352810+ntphat11296@users.noreply.github.com>
2017-12-13 23:12:08 +0700

    a
    aa

---
+++ BT3/BT3SSLVerify-master/BT3SSL_UDP_Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace BT3SSL_UDP_Server
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Encrypting en = new Encrypting();
+
+			Console.WriteLine(Encoding.ASCII.GetString(en.decrypt(en.encrypt("vpdoan", "duongthanhdung96"), Encoding.ASCII.GetBytes("duongthanhdung96"))));
+		}
+	}
+}
